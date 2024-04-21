c559320d2a7d90ca6086023c93d57b8637c087ec
Никита Соколов <112575443+npsokolov@users.noreply.github.com>
2024-04-21 23:13:18 +0300

    Update TREAP.cpp

--- trees/TREAP.cpp
+++ trees/TREAP.cpp
@@ -1,138 +1,149 @@
-#include <optional>
-#include <vector>
-#include <random>
-#include <iostream>
+/*#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define _CRT_SECURE_NO_WARNINGS*/
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<array>
+#include<unordered_set>
+#include<unordered_map>
+#include<cstring>
+#include<string>
+#include<memory>
+#include<iomanip>
+#include<cassert>
+#include<cmath>
+#include<random>
+#include<algorithm>
+#include<chrono>
+#include <unistd.h>
+#include <complex>
+
 using namespace std;
-
-int GeneratePriority();
-
-struct TreapNode
-{
-    int key,prior,left,right;
-    TreapNode(int key_) : key(key_), left(-1), right(-1), prior(GeneratePriority()) {}
+#define endl '\n'
+#define int long long
+#define ld long double
+struct TTreapNode {
+    int Priority;
+    int Value;
+    int Num;
+    long long Sum;
+    int Left;
+    int Right;
 };
 
-class Treap
-{
+struct TTreapStorage {
+    std::vector<TTreapNode> Nodes;
+
+    int AllocateNode(int value, int num, int left, int right){
+        static std::mt19937 Generator(42);
+        auto priority = std::uniform_int_distribution<int>()(Generator);
+
+        Nodes.emplace_back();
+        Nodes.back().Priority = priority;
+        Nodes.back().Value = value;
+        Nodes.back().Num = num;
+        Nodes.back().Sum = num;
+        Nodes.back().Left = left;
+        Nodes.back().Right = right;
+        InitCount(Nodes.size() - 1);
+
+        return Nodes.size() - 1;
+    }
+
+    void InitCount(int index){
+        if (index != -1) {
+            Nodes[index].Sum = Nodes[index].Num;
+            if (Nodes[index].Left != -1) {
+                Nodes[index].Sum += Nodes[Nodes[index].Left].Sum;
+            }
+            if (Nodes[index].Right != -1) {
+                Nodes[index].Sum += Nodes[Nodes[index].Right].Sum;
+            }
+        }
+    }
+};
+
+class TTreap {
 public:
-    void Insert(int key);
-    bool Find(int key);
-    optional<int> FindLowerBound(int key);
-private:
-    int Root=-1;
-    vector<TreapNode> Nodes_;
-    pair<int,int> Split(int cur, int key);
-    int Merge(int left,int right);
+    TTreap() : Storage_(new TTreapStorage()), Index_(-1) {}
+
+    TTreap(std::shared_ptr<TTreapStorage> storage, int index) : Storage_(std::move(storage)), Index_(index) {}
+
+    std::shared_ptr<TTreapStorage> Storage_;
+
+    int Index_;
+
+    // #right can be changed and must not be used after the call.
+    void Append(TTreap right){
+        assert(Storage_.get() == right.Storage_.get());
+        Index_ = Merge(Index_, right.Index_);
+    }
+
+    int Merge(int left, int right){
+        if (left == -1)return right;
+        if (right == -1)return left;
+        auto &nodes = Storage_->Nodes;
+        if (nodes[left].Priority > nodes[right].Priority) {
+            nodes[left].Right = Merge(nodes[left].Right, right);
+            Storage_->InitCount(left);
+            return left;
+        } else {
+            nodes[right].Left = Merge(left, nodes[right].Left);
+            Storage_->InitCount(right);
+            return right;
+        }
+    }
+
+    std::pair<int, int> Split(int current, int key){
+        if (current == -1) return std::make_pair(-1, -1);
+        auto &nodes = Storage_->Nodes;
+        if (nodes[current].Value <= key) {
+            auto [left, right] = Split(nodes[current].Right, key);
+            nodes[current].Right = left;
+            Storage_->InitCount(current);
+            return std::make_pair(current, right);
+        } else {
+            auto [left, right] = Split(nodes[current].Left, key);
+            nodes[current].Left = right;
+            Storage_->InitCount(current);
+            return std::make_pair(left, current);
+        }
+    }
+
+    void Insert(int key,int num){
+        auto [left, right] = Split(Index_, key - 1);
+        auto &nodes = Storage_->Nodes;
+        int index = Storage_->AllocateNode(key, num, -1, -1);
+        left = Merge(left, index);
+        Index_ = Merge(left, right);
+    }
 };
 
-int GeneratePriority()
-{
-    static mt19937 Generator(42);
-    return uniform_int_distribution<int>()(Generator);
-}
-
-pair<int,int> Treap::Split(int cur, int key) {
-    if (cur==-1){
-        return {-1,-1};
-    }
-    if (Nodes_[cur].key>key){
-        auto [l,r] = Split(Nodes_[cur].left,key);
-        Nodes_[cur].left=r;
-        return {l,cur};
-    }else{
-        auto [l,r] = Split(Nodes_[cur].right,key);
-        Nodes_[cur].right=l;
-        return {cur,r};
-    }
-}
-
-int Treap::Merge(int left, int right) {
-    if (left==-1){
-        return right;
-    }
-    if (right==-1){
-        return left;
-    }
-    if (Nodes_[left].prior<Nodes_[right].prior){
-        Nodes_[right].left=Merge(left,Nodes_[right].left);
-        return right;
-    }else{
-        Nodes_[left].right=Merge(Nodes_[left].right,right);
-        return left;
-    }
-}
-
-void Treap::Insert(int key) {
-    auto [lm,r] = Split(Root,key);
-    auto [l,m] = Split(lm,key-1);
-    if (m==-1) {
-        Nodes_.emplace_back(TreapNode(key));
-        Root = Merge(l, Merge(Nodes_.size() - 1, r));
-    }else{
-        Root = Merge(l,Merge(m,r));
-    }
-}
 
 
-bool Treap::Find(int key){
-    bool flag=true;
-    auto [lm,r] = Split(Root,key);
-    auto [l,mid] = Split(lm,key-1);
-    auto result = mid != -1;
-    Root = Merge(l, Merge(mid, r));
-    return result;
 
-}
-
-optional<int> Treap::FindLowerBound(int key) {
-    int cur = Root;
-    int res = -1;
-    while (cur>=0){
-        if (Nodes_[cur].key==key){
-            res=cur;
-            break;
-        }else if (Nodes_[cur].key>key){
-            res=cur;
-            cur=Nodes_[cur].left;
-        }else{
-            cur=Nodes_[cur].right;
-        }
-    }
-    return res ==-1 ? nullopt : optional<int>(Nodes_[res].key);
-}
-
-const int mod = 1e9;
-
-
-int main(){
-    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    int n,x,lastx;
-    char c;
-    Treap tree;
+void solve() {
+    int n, m;
     cin >> n;
-    char last='+';
-    while (n--){
-        cin >> c >> x;
-        if (c=='+'){
-            if (last=='?') {
-                tree.Insert((x+lastx)%mod);
-            }else{
-                tree.Insert(x);
-            }
-        }else{
-            auto f = tree.FindLowerBound(x);
-            if (f){
-                lastx=f.value();
-                cout << f.value() << endl;
-            }else{
-                cout << -1 << endl;
-                lastx=-1;
-            }
-        }
-        last=c;
-    }
-    cout.flush();
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+    int q = 1;
+    //cin >> q;
+    while (q--)
+        solve();
     return 0;
 }
